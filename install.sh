1cdfaf25639b44377e69d5b8722e2a037cd13379
Noah Heller <dev.noahheller@pm.me>
2025-07-17 21:26:54 -0400

    Has not been tested. Assets coppied from Grey and color palette is ready for testing

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ REPO_DIR="$(dirname "$(readlink -m "${0}")")"
 source "${REPO_DIR}/core.sh"
 
 usage() {
-cat << EOF
+  cat <<EOF
 Usage: $0 [OPTION]...
 
 OPTIONS:
@@ -65,369 +65,373 @@ lcolors=()
 
 while [[ "$#" -gt 0 ]]; do
   case "${1:-}" in
-    -d|--dest)
-      dest="$2"
-      mkdir -p "$dest"
-      shift 2
-      ;;
-    -n|--name)
-      _name="$2"
-      shift 2
-      ;;
-    -r|--remove|-u|--uninstall)
-      remove="true"
-      shift
-      ;;
-    -l|--libadwaita)
-      libadwaita="true"
-      shift
-      ;;
-    -f|--fixed)
-      fixed="true"
-      shift
-      ;;
-    --round)
-      round="true"
-      corner="$2"
-      echo -e "Change round corner ${corner} value ..."
-      shift 2
-      ;;
-    --shell)
-      shift
-      for shell in $@; do
-        case "$shell" in
-          38)
-            shell="38"
-            shift
-            ;;
-          40)
-            shell="40"
-            shift
-            ;;
-          42)
-            shell="42"
-            shift
-            ;;
-          44)
-            shell="44"
-            shift
-            ;;
-          46)
-            shell="46"
-            shift
-            ;;
-          47)
-            shell="47"
-            shift
-            ;;
-          48)
-            shell="48"
-            shift
-            ;;
-          -*)
-            break
-            ;;
-          *)
-            echo "ERROR: Unrecognized shell variant '$1'."
-            echo "Try '$0 --help' for more information."
-            exit 1
-            ;;
-        esac
-      done
-      ;;
-    --tweaks)
-      shift
-      for variant in $@; do
-        case "$variant" in
-          solid)
-            opacity="solid"
-            echo -e "Install solid version ..."
-            shift
-            ;;
-          compact)
-            panel="compact"
-            echo -e "Install compact panel version ..."
-            shift
-            ;;
-          black)
-            blackness="true"
-            echo -e "Install black version ..."
-            shift
-            ;;
-          primary)
-            primary="true"
-            echo "Change radio and check assets color ..."
-            shift
-            ;;
-          macos)
-            macstyle="true"
-            echo -e "Install macOS style window button version ..."
-            shift
-            ;;
-          submenu)
-            submenu="true"
-            echo -e "Install with themed sub-menus ..."
-            shift
-            ;;
-          nord)
-            nord="true"
-            ctype="-Nord"
-            echo -e "Install nord colorscheme ..."
-            shift
-            ;;
-          dracula)
-            dracula="true"
-            ctype="-Dracula"
-            echo -e "Install dracula colorscheme ..."
-            shift
-            ;;
-          dock)
-            dockfix="true"
-            echo -e "\nFix 'dash-to-dock' or 'ubuntu-dock' style ..."
-            shift
-            ;;
-          -*)
-            break
-            ;;
-          *)
-            echo "ERROR: Unrecognized tweaks variant '$1'."
-            echo "Try '$0 --help' for more information."
-            exit 1
-            ;;
-        esac
-      done
-      ;;
-    -t|--theme)
-      accent='true'
-      shift
-      for variant in "$@"; do
-        case "$variant" in
-          default)
-            themes+=("${THEME_VARIANTS[0]}")
-            shift
-            ;;
-          purple)
-            themes+=("${THEME_VARIANTS[1]}")
-            shift
-            ;;
-          pink)
-            themes+=("${THEME_VARIANTS[2]}")
-            shift
-            ;;
-          red)
-            themes+=("${THEME_VARIANTS[3]}")
-            shift
-            ;;
-          orange)
-            themes+=("${THEME_VARIANTS[4]}")
-            shift
-            ;;
-          yellow)
-            themes+=("${THEME_VARIANTS[5]}")
-            shift
-            ;;
-          green)
-            themes+=("${THEME_VARIANTS[6]}")
-            shift
-            ;;
-          teal)
-            themes+=("${THEME_VARIANTS[7]}")
-            shift
-            ;;
-          grey)
-            themes+=("${THEME_VARIANTS[8]}")
-            shift
-            ;;
-          all)
-            themes+=("${THEME_VARIANTS[@]}")
-            shift
-            ;;
-          -*)
-            break
-            ;;
-          *)
-            echo "ERROR: Unrecognized theme variant '$1'."
-            echo "Try '$0 --help' for more information."
-            exit 1
-            ;;
-        esac
-      done
-      ;;
-    -c|--color)
-      shift
-      for variant in "$@"; do
-        case "$variant" in
-          standard)
-            colors+=("${COLOR_VARIANTS[0]}")
-            lcolors+=("${COLOR_VARIANTS[0]}")
-            shift
-            ;;
-          light)
-            colors+=("${COLOR_VARIANTS[1]}")
-            lcolors+=("${COLOR_VARIANTS[1]}")
-            shift
-            ;;
-          dark)
-            colors+=("${COLOR_VARIANTS[2]}")
-            lcolors+=("${COLOR_VARIANTS[2]}")
-            shift
-            ;;
-          -*)
-            break
-            ;;
-          *)
-            echo "ERROR: Unrecognized color variant '$1'."
-            echo "Try '$0 --help' for more information."
-            exit 1
-            ;;
-        esac
-      done
-      ;;
-    -s|--size)
-      shift
-      for variant in "$@"; do
-        case "$variant" in
-          standard)
-            sizes+=("${SIZE_VARIANTS[0]}")
-            shift
-            ;;
-          compact)
-            sizes+=("${SIZE_VARIANTS[1]}")
-            shift
-            ;;
-          -*)
-            break
-            ;;
-          *)
-            echo "ERROR: Unrecognized size variant '$1'."
-            echo "Try '$0 --help' for more information."
-            exit 1
-            ;;
-        esac
-      done
-      ;;
-    -i|--icon)
-      activities='icon'
-      shift
-      for icons in "$@"; do
-        case "$icons" in
-          default)
-            icon='-default'
-            shift
-            ;;
-          apple)
-            icon='-apple'
-            shift
-            ;;
-          simple)
-            icon='-simple'
-            shift
-            ;;
-          gnome)
-            icon='-gnome'
-            shift
-            ;;
-          ubuntu)
-            icon='-ubuntu'
-            shift
-            ;;
-          arch)
-            icon='-arch'
-            shift
-            ;;
-          manjaro)
-            icon='-manjaro'
-            shift
-            ;;
-          fedora)
-            icon='-fedora'
-            shift
-            ;;
-          debian)
-            icon='-debian'
-            shift
-            ;;
-          void)
-            icon='-void'
-            shift
-            ;;
-          opensuse)
-            icon='-opensuse'
-            shift
-            ;;
-          popos)
-            icon='-popos'
-            shift
-            ;;
-          mxlinux)
-            icon='-mxlinux'
-            shift
-            ;;
-          zorin)
-            icon='-zorin'
-            shift
-            ;;
-          endeavouros)
-            icon='-endeavouros'
-            shift
-            ;;
-          tux)
-            icon='-tux'
-            shift
-            ;;
-          nixos)
-            icon='-nixos'
-            shift
-            ;;
-          gentoo)
-            icon='-gentoo'
-            shift
-            ;;
-          budgie)
-            icon='-budgie'
-            shift
-            ;;
-          solus)
-            icon='-solus'
-            shift
-            ;;
-          -*)
-            break
-            ;;
-          *)
-            echo "ERROR: Unrecognized icon variant '$1'."
-            echo "Try '$0 --help' for more information."
-            exit 1
-            ;;
-        esac
-        echo "Install $icons icon for gnome-shell panel..."
-      done
-      ;;
-    -h|--help)
-      usage
-      exit 0
-      ;;
-    *)
-      echo "ERROR: Unrecognized installation option '$1'."
-      echo "Try '$0 --help' for more information."
-      exit 1
-      ;;
+  -d | --dest)
+    dest="$2"
+    mkdir -p "$dest"
+    shift 2
+    ;;
+  -n | --name)
+    _name="$2"
+    shift 2
+    ;;
+  -r | --remove | -u | --uninstall)
+    remove="true"
+    shift
+    ;;
+  -l | --libadwaita)
+    libadwaita="true"
+    shift
+    ;;
+  -f | --fixed)
+    fixed="true"
+    shift
+    ;;
+  --round)
+    round="true"
+    corner="$2"
+    echo -e "Change round corner ${corner} value ..."
+    shift 2
+    ;;
+  --shell)
+    shift
+    for shell in $@; do
+      case "$shell" in
+      38)
+        shell="38"
+        shift
+        ;;
+      40)
+        shell="40"
+        shift
+        ;;
+      42)
+        shell="42"
+        shift
+        ;;
+      44)
+        shell="44"
+        shift
+        ;;
+      46)
+        shell="46"
+        shift
+        ;;
+      47)
+        shell="47"
+        shift
+        ;;
+      48)
+        shell="48"
+        shift
+        ;;
+      -*)
+        break
+        ;;
+      *)
+        echo "ERROR: Unrecognized shell variant '$1'."
+        echo "Try '$0 --help' for more information."
+        exit 1
+        ;;
+      esac
+    done
+    ;;
+  --tweaks)
+    shift
+    for variant in $@; do
+      case "$variant" in
+      solid)
+        opacity="solid"
+        echo -e "Install solid version ..."
+        shift
+        ;;
+      compact)
+        panel="compact"
+        echo -e "Install compact panel version ..."
+        shift
+        ;;
+      black)
+        blackness="true"
+        echo -e "Install black version ..."
+        shift
+        ;;
+      primary)
+        primary="true"
+        echo "Change radio and check assets color ..."
+        shift
+        ;;
+      macos)
+        macstyle="true"
+        echo -e "Install macOS style window button version ..."
+        shift
+        ;;
+      submenu)
+        submenu="true"
+        echo -e "Install with themed sub-menus ..."
+        shift
+        ;;
+      nord)
+        nord="true"
+        ctype="-Nord"
+        echo -e "Install nord colorscheme ..."
+        shift
+        ;;
+      dracula)
+        dracula="true"
+        ctype="-Dracula"
+        echo -e "Install dracula colorscheme ..."
+        shift
+        ;;
+      dock)
+        dockfix="true"
+        echo -e "\nFix 'dash-to-dock' or 'ubuntu-dock' style ..."
+        shift
+        ;;
+      -*)
+        break
+        ;;
+      *)
+        echo "ERROR: Unrecognized tweaks variant '$1'."
+        echo "Try '$0 --help' for more information."
+        exit 1
+        ;;
+      esac
+    done
+    ;;
+  -t | --theme)
+    accent='true'
+    shift
+    for variant in "$@"; do
+      case "$variant" in
+      default)
+        themes+=("${THEME_VARIANTS[0]}")
+        shift
+        ;;
+      purple)
+        themes+=("${THEME_VARIANTS[1]}")
+        shift
+        ;;
+      pink)
+        themes+=("${THEME_VARIANTS[2]}")
+        shift
+        ;;
+      red)
+        themes+=("${THEME_VARIANTS[3]}")
+        shift
+        ;;
+      orange)
+        themes+=("${THEME_VARIANTS[4]}")
+        shift
+        ;;
+      yellow)
+        themes+=("${THEME_VARIANTS[5]}")
+        shift
+        ;;
+      green)
+        themes+=("${THEME_VARIANTS[6]}")
+        shift
+        ;;
+      teal)
+        themes+=("${THEME_VARIANTS[7]}")
+        shift
+        ;;
+      grey)
+        themes+=("${THEME_VARIANTS[8]}")
+        shift
+        ;;
+      darkroast)
+        themes+=("${THEME_VARIANTS[9]}")
+        shift
+        ;;
+      all)
+        themes+=("${THEME_VARIANTS[@]}")
+        shift
+        ;;
+      -*)
+        break
+        ;;
+      *)
+        echo "ERROR: Unrecognized theme variant '$1'."
+        echo "Try '$0 --help' for more information."
+        exit 1
+        ;;
+      esac
+    done
+    ;;
+  -c | --color)
+    shift
+    for variant in "$@"; do
+      case "$variant" in
+      standard)
+        colors+=("${COLOR_VARIANTS[0]}")
+        lcolors+=("${COLOR_VARIANTS[0]}")
+        shift
+        ;;
+      light)
+        colors+=("${COLOR_VARIANTS[1]}")
+        lcolors+=("${COLOR_VARIANTS[1]}")
+        shift
+        ;;
+      dark)
+        colors+=("${COLOR_VARIANTS[2]}")
+        lcolors+=("${COLOR_VARIANTS[2]}")
+        shift
+        ;;
+      -*)
+        break
+        ;;
+      *)
+        echo "ERROR: Unrecognized color variant '$1'."
+        echo "Try '$0 --help' for more information."
+        exit 1
+        ;;
+      esac
+    done
+    ;;
+  -s | --size)
+    shift
+    for variant in "$@"; do
+      case "$variant" in
+      standard)
+        sizes+=("${SIZE_VARIANTS[0]}")
+        shift
+        ;;
+      compact)
+        sizes+=("${SIZE_VARIANTS[1]}")
+        shift
+        ;;
+      -*)
+        break
+        ;;
+      *)
+        echo "ERROR: Unrecognized size variant '$1'."
+        echo "Try '$0 --help' for more information."
+        exit 1
+        ;;
+      esac
+    done
+    ;;
+  -i | --icon)
+    activities='icon'
+    shift
+    for icons in "$@"; do
+      case "$icons" in
+      default)
+        icon='-default'
+        shift
+        ;;
+      apple)
+        icon='-apple'
+        shift
+        ;;
+      simple)
+        icon='-simple'
+        shift
+        ;;
+      gnome)
+        icon='-gnome'
+        shift
+        ;;
+      ubuntu)
+        icon='-ubuntu'
+        shift
+        ;;
+      arch)
+        icon='-arch'
+        shift
+        ;;
+      manjaro)
+        icon='-manjaro'
+        shift
+        ;;
+      fedora)
+        icon='-fedora'
+        shift
+        ;;
+      debian)
+        icon='-debian'
+        shift
+        ;;
+      void)
+        icon='-void'
+        shift
+        ;;
+      opensuse)
+        icon='-opensuse'
+        shift
+        ;;
+      popos)
+        icon='-popos'
+        shift
+        ;;
+      mxlinux)
+        icon='-mxlinux'
+        shift
+        ;;
+      zorin)
+        icon='-zorin'
+        shift
+        ;;
+      endeavouros)
+        icon='-endeavouros'
+        shift
+        ;;
+      tux)
+        icon='-tux'
+        shift
+        ;;
+      nixos)
+        icon='-nixos'
+        shift
+        ;;
+      gentoo)
+        icon='-gentoo'
+        shift
+        ;;
+      budgie)
+        icon='-budgie'
+        shift
+        ;;
+      solus)
+        icon='-solus'
+        shift
+        ;;
+      -*)
+        break
+        ;;
+      *)
+        echo "ERROR: Unrecognized icon variant '$1'."
+        echo "Try '$0 --help' for more information."
+        exit 1
+        ;;
+      esac
+      echo "Install $icons icon for gnome-shell panel..."
+    done
+    ;;
+  -h | --help)
+    usage
+    exit 0
+    ;;
+  *)
+    echo "ERROR: Unrecognized installation option '$1'."
+    echo "Try '$0 --help' for more information."
+    exit 1
+    ;;
   esac
 done
 
-if [[ "${#themes[@]}" -eq 0 ]] ; then
+if [[ "${#themes[@]}" -eq 0 ]]; then
   themes=("${THEME_VARIANTS[0]}")
 fi
 
-if [[ "${#colors[@]}" -eq 0 ]] ; then
+if [[ "${#colors[@]}" -eq 0 ]]; then
   colors=("${COLOR_VARIANTS[@]}")
 fi
 
-if [[ "${#sizes[@]}" -eq 0 ]] ; then
+if [[ "${#sizes[@]}" -eq 0 ]]; then
   sizes=("${SIZE_VARIANTS[@]}")
 fi
 
-if [[ "${#lcolors[@]}" -eq 0 ]] ; then
+if [[ "${#lcolors[@]}" -eq 0 ]]; then
   lcolors=("${COLOR_VARIANTS[1]}")
 fi
 
